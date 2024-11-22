974c25969e348eb30834251b8e700ba490780b09
Mates061011 <161950987+Mates061011@users.noreply.github.com>
2024-11-22 17:01:10 +0100

    logout

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,12 +1,22 @@
 // src/components/Navigation/Navigation.tsx
-import { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import './navbar.css';
 import Logo from "../../assets/logo.svg";
 import { AuthContext } from '../../context/AuthContext'; // Import the AuthContext
 
 const Navigation = () => {
   const authContext = useContext(AuthContext); // Get the auth context
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    // Remove the token from localStorage and reset the user context
+    localStorage.removeItem('token');
+    if (authContext) {
+      authContext.setUser(null); // Reset user context to null
+    }
+    navigate('/Login'); // Redirect to Login page after logout
+  };
 
   return (
     <nav>
@@ -25,6 +35,12 @@ const Navigation = () => {
             <Link to="/Login">BETA Testing</Link> // Show "Login" link if not logged in
           )}
         </div>
+        {/* Render logout button if the user is logged in */}
+        {authContext?.user && (
+          <button onClick={handleLogout} className="logoutButton">
+            Logout
+          </button>
+        )}
       </div>
     </nav>
   );
